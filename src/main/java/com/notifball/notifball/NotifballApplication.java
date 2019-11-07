4c50d422f7d3c81206ae1afc342d79b64ce2509a
Iwo Czerniawski <iwo.czerniawski@allegro.pl>
2019-11-07 18:54:53 +0100

    initial commit

---
+++ src/main/java/com/notifball/notifball/NotifballApplication.java
@@ -0,0 +1,13 @@
+package com.notifball.notifball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotifballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/notifball/notifball/NotifballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notifball.notifball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
